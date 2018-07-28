82e0a19702a532101eac5d3b5a21d4e44be01d28
Zyzzyva038 <xingboni@yahoo.com>
2018-07-28 15:51:06 +0800

    First added
    After finished ;D

---
+++ dicts/write.py
@@ -0,0 +1,5 @@
+f = open('dict.dic', 'r')
+l = f.readlines()
+for line in l:
+    print(line.strip())
+    
---
+++ reset_high_score.py
@@ -0,0 +1,7 @@
+def main():
+	f = open("text\\high_score.txt", "w")
+	f.write("0")
+	f.close()
+	
+if __name__ == "__main__":
+	main()
